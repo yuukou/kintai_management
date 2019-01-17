07e45fb1247c7c61a7dd3ebedc22bc6f04ee3c80
田中　優晃 <yuukou.triplejump0219@gmail.com>
2019-01-17 18:26:22 +0900

    日本語に対応になっていない場所を日本語対応にした

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tanakayuko
+ * Date: 2019/01/17
+ * Time: 17:39
+ */
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+}
