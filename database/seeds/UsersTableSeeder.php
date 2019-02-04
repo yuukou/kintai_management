9eea457d54da262d3b1b51be34e711872e690911
田中　優晃 <yuukou.triplejump0219@gmail.com>
2019-02-04 10:53:40 +0900

    seederを作成した

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'kintai試験',
+            'ip_address' => '127.0.0.1',
+        ]);
+    }
+}
