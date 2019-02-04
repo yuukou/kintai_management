41b467954c42840c1848362e70910882fdd62f4b
田中　優晃 <yuukou.triplejump0219@gmail.com>
2019-02-04 23:26:18 +0900

    出勤ボタンと退勤ボタンを一つずつ表示させるようにした

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -2,21 +2,26 @@
 @section('content')
     <div id="realTimeClockArea" class="clock"></div>
     <div class="btns">
+        @if(! $arrivedFlg)
         <div class="btn_wrapper">
             {{ Form::open(['url' => route('postStoreArrive', ['user' => $user->id]), 'method' => 'post', 'class' => 'form-horizontal']) }}
             {{ csrf_field() }}
             {{ Form::hidden('attendance', 'arrive') }}
-            {{ Form::submit('🌞', ['class' => 'btn arrive'.(! $arrivedFlg ? ' on_btn': ''), 'id' => 'arrive', $arrivedFlg ? "disabled='disabled'" : '']) }}
+            {{ Form::submit('🌞', ['class' => 'btn on_btn', 'id' => 'arrive']) }}
             {{ Form::close() }}
             <p class="arrive_description">出社</p>
         </div>
+        @elseif($arrivedFlg && ! $leftFlg)
         <div class="btn_wrapper">
             {{ Form::open(['url' => route('postStoreLeave', ['user' => $user->id]), 'method' => 'post', 'class' => 'form-horizontal']) }}
             {{ csrf_field() }}
             {{ Form::hidden('attendance', 'leave') }}
-            {{ Form::submit('🌚', ['class' => "btn leave".($arrivedFlg && ! $leftFlg ? ' on_btn': '' ), 'id' => 'leave', ! $arrivedFlg || $leftFlg ? "disabled='disabled'" : '']) }}
+            {{ Form::submit('🌚', ['class' => "btn on_btn", 'id' => 'leave']) }}
             {{ Form::close() }}
             <p class="leave_description">退社</p>
         </div>
+        @else
+            <p class="good_bye_description">本日も一日お疲れ様でした👼👼👼</p>
+        @endif
     </div>
 @endsection
